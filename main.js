1332ea9c1216cffe4d853cff1a47bf8641120b3b
ananya-naveen <84455616+ananya-naveen@users.noreply.github.com>
2022-01-07 17:30:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftWristX=0;
+rightWristX=0;
+difference=0;
+
 function setup(){
     video=createCapture(VIDEO);
     video.size(550,500);
@@ -15,9 +19,16 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
+
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX-rightWristX);
     }
 }
 
 function draw(){
     background("#FCA311");
+    textSize(difference);
+    fill("#ff0000")
+    text("ANANYA",40,300);
 }
